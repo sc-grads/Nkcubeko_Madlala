a8adf9648245a1d871821ee9ba47a6e8dcfd71f3
Madlala Nkcubeko <madlala.nkcubeko@sambeconsulting.com>
2023-02-21 12:57:35 +0200

    Building Concurrent Applications using Async IO

---
+++ NetworkAutomation/netmiko.py
@@ -0,0 +1,17 @@
+# from netmiko import connectHandler
+# cisco_device = {
+#     'device_type': 'cisco_ios',
+#     'host': '10.1.1.10',
+#     'username': 'v1',
+#     'password': 'cisco',
+#     'port': '20',
+#     'secret': 'cisco',
+#     'verbose': True
+# }
+#
+# connection = connectHandler(**cisco_device)
+# output = connection.send_command('sh ip int brief')
+# print(output)
+#
+# print('Closing connection')
+# connection.disconnect()
---
+++ NetworkAutomation/paramiko_connect.py
@@ -0,0 +1,36 @@
+import paramiko
+from scp import SCPClient
+ssh_client = paramiko.SSHClient()
+print(type(ssh_client))
+
+ssh_client.load_system_host_keys()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# ssh_client.connect(hostname='10.1.1.10', port= '22', username='v1', password='cisco' look_for_keys=False, allow_agent=False)
+# print(ssh_client.get_transport().is_active())
+#
+# scp = SCPClient(ssh_client.get_transport())
+#
+# scp.put('dictionary1', recursive=True, remote_path='/tmp')
+#
+# scp.get('/etc/passwd', 'passwd')
+# scp.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
