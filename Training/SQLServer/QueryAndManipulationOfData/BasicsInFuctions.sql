20607f40c165c7ce50c3a528e5e54c5fd256876b
Madlala Nkcubeko <madlala.nkcubeko@sambeconsulting.com>
2023-03-02 15:59:59 +0200

    Query and manipulation of data using SQL Commands

---
+++ Training/SQLServer/QueryAndManipulationOfData/BasicsInFuctions.sql
@@ -0,0 +1,24 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+
+
+SELECT MAX(rate) 
+	FROM [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
+
+SELECT MAX(rate)
+	FROM [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
+
+SELECT COUNT(*)
+	FROM [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
+
+SELECT SUM(Rate)
+	FROM [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
+
+print upper('iBAnDlalEtHu')
+print lower('iBAnDlalEtHu')
+
+print convert(int,14.8679)
+
+print getdate()
+print day(getdate())
+print month(getdate())
+print year(getdate())
