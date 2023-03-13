2ad6496264deb698a9fc25cc959d3fff66f44eaf
Madlala Nkcubeko <madlala.nkcubeko@sambeconsulting.com>
2023-03-13 16:09:58 +0200

    Working with Transact-SQL

---
+++ Training/TransactSQL/StringsInDetail.sql
@@ -0,0 +1,27 @@
+﻿-- char - ASCII - 1 byte
+-- varchar - ASCII - 1 byte
+-- nchar - UNICODE - 2 bytes
+-- nvarchar - UNICODE - 2 bytes
+
+-- ASCII: A-Z, a-z, 0-9  
+--       65   97   48   32  128 (0-255)
+
+DECLARE @chrMyCharacters as char(10)
+
+set @chrMyCharacters = 'hello'
+
+SELECT @chrMyCharacters as myString, len(@chrMyCharacters) as MyLength, DATALENGTH(@chrMyCharacters) as MyDataLength
+
+-- Always prefix nchar and nvarchar values with N (capital N).
+
+DECLARE @chrASCII as varchar(10) = 'hellothere'
+
+DECLARE @chrUNICODE as nvarchar(10) = N'helloϞ'
+
+select left(@chrASCII,2) as myASCII, right(@chrUNICODE,2) as myUNICODE
+select substring(@chrASCII,3,2) as middleletters
+select ltrim(rtrim(@chrASCII)) as myTRIM
+select replace(@chrASCII,'l','L') as myReplace
+select upper(@chrASCII) as myUPPER
+select lower(@chrASCII) as myLOWER
+
